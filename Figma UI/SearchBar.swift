1e4b13f21b3131336de2f5bf299a175e42fb38c7
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-10-19 20:40:26 -0600

    added sliding animation to the search bar toggle

--- Figma UI/SearchBar.swift
+++ Figma UI/SearchBar.swift
@@ -8,45 +8,61 @@
 import SwiftUI
 
 struct SearchBar: View {
+    @State private var isLeftSelected: Bool = true
+    private let buttonWidth: CGFloat = 100
+    private let textHeight: CGFloat = 45
+    private let buttonHeight: CGFloat = 40
     
-    @State private var isLocalSelected: Bool = true
     var body: some View {
         HStack {
             Image(systemName: "magnifyingglass")
             Spacer()
             
-            HStack(spacing: 0) {
-                        Button(action: {
-                            isLocalSelected = true
-                        }) {
-                            Text("Stay Local")
-                                .padding(.vertical, 10)
-                                .padding(.horizontal)
-                                .foregroundColor(Color.black)
-                                .background(isLocalSelected ? Color(red: 0.95, green: 0.85, blue: 0.8) : Color(red: 0.8, green: 0.7, blue: 0.6))
-                                .cornerRadius(20)
+            ZStack(alignment: isLeftSelected ? .leading : .trailing) {
+                // Highlighted Rectangle
+                Rectangle()
+                    .frame(width: buttonWidth, height: buttonHeight)
+                    .foregroundColor(Color(red: 0.95, green: 0.85, blue: 0.8))
+                    .cornerRadius(20)
+                    .transition(.slide)
+                    .padding(.horizontal, 5)
+                
+                // Buttons
+                HStack(spacing: 0) {
+                    Button(action: {
+                        withAnimation {
+                            isLeftSelected = true
                         }
-                        .frame(maxWidth: .infinity)
-                        
-                        Button(action: {
-                            isLocalSelected = false
-                        }) {
-                            Text("Go Global")
-                                .padding(.vertical, 10)
-                                .padding(.horizontal)
-                                .foregroundColor(Color.black)
-                                .background(!isLocalSelected ? Color(red: 0.95, green: 0.85, blue: 0.8) : Color(red: 0.8, green: 0.7, blue: 0.6))
-                                .cornerRadius(20)
-                        }
-                        .frame(maxWidth: .infinity)
+                    }) {
+                        Text("Stay Local")
+                            .frame(width: buttonWidth, height: textHeight)
+                            .background(Color.clear)
+                            .foregroundColor(.black)
+                            .padding(.horizontal, 5)
                     }
-                    .background(Color(red: 0.8, green: 0.7, blue: 0.6))
-                    .cornerRadius(25)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(Color.black.opacity(0.2), lineWidth: 1)
-                    )
-                    .padding()
+                    
+                    
+                    Button(action: {
+                        withAnimation {
+                            isLeftSelected = false
+                        }
+                    }) {
+                        Text("Go Global")
+                            .frame(width: buttonWidth, height: textHeight)
+                            .background(Color.clear)
+                            .foregroundColor(.black)
+                            .padding(.horizontal, 5)
+                    }
+                } // Background for buttons
+                
+            }
+            .background(Color(red: 0.8, green: 0.7, blue: 0.6))
+            .cornerRadius(25)
+            .overlay(
+                RoundedRectangle(cornerRadius: 25)
+                    .stroke(Color.black.opacity(0.2), lineWidth: 1)
+            )
+            .padding()
             
             Spacer()
             Image("ProfilePicture")
